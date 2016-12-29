17d2c4cd6385a59b72d07051b939f31580938a69
tobyp <tobyp@tobyp.net>
2016-12-29 16:13:01 +0100

    Add counter

---
+++ core/src/main/java/com/example/hackathon/core/model/Counter.java
@@ -0,0 +1,24 @@
+package com.example.hackathon.core.model;
+
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by tobyp on 12/29/16.
+ */
+public class Counter extends Entity {
+    protected String onZero;
+    protected int count;
+
+    public Counter(int init_count, String onZero) {
+        super(new Vector2());
+        this.count = init_count;
+        this.onZero = onZero;
+    }
+
+    public int dec(World world) {
+        this.count--;
+        if (this.count == 0) {
+            world.runScript(null, onZero);
+        }
+    }
+}
